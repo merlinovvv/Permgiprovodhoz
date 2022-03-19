c2edfffa2a0a0813e84d9a2b46c8ce844ee68a47
maerlinovvv <merlinovandrej@gmail.com>
2022-03-19 19:55:34 +0200

    new

--- js/script.js
+++ js/script.js
@@ -28,3 +28,28 @@ $(function() {
       }
     });
    });
+$(function(){
+    const mediaQuery = window.matchMedia('(max-width: 480px)')
+   let elem = $('.page__scheme');
+   let elem2 = $('.scheme__grid');
+   let elem3 = $('.scheme__box');
+    if (mediaQuery.matches) {
+        elem.addClass('swiper-container');
+        elem2.addClass('swiper-wrapper');
+        elem3.addClass('swiper-slide');
+        new Swiper('.page__scheme',{
+            navigation:{
+                nextEl:'.swiper-button-next',
+                prevEl:'.swiper-button-prev'
+            },
+        });
+    }
+})
+$(function(){
+    const mediaQuery = window.matchMedia('(min-width: 480px)')
+    if(mediaQuery.matches){
+        elem.removeClass('swiper-container');
+        elem2.removeClass('swiper-wrapper');
+        elem3.removeClass('swiper-slide');
+    }
+})
